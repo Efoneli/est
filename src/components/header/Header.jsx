d8298c2e19ed323f7f84c6bfb82109c7f27e2d55
Efoneli <abbaefoneli@gmail.com>
2023-11-03 21:31:55 -0400

    setting up

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+      <h1>logo is here</h1>
+      <div>
+      <p>Deliver to:</p>
+      <div>
+        current location <span>Mohammed Bus stand, dhak</span>
+      </div>
+      </div>
+
+      <div>
+        Search
+        <button>
+          Login
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default Header
---
+++ src/routes/Routers.jsx
@@ -0,0 +1,15 @@
+import Home from "../pages/Home";
+
+
+import { Routes, Route } from "react-router-dom";
+
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+};
+
+export default Routers;
