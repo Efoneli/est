53a8d8cbb678d7818348c9ffbc4301180900efdc
Efoneli <abbaefoneli@gmail.com>
2023-11-06 08:12:55 -0500

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
+import { BiSolidShoppingBag } from "react-icons/bi";
+import { GrBike } from "react-icons/gr";
+
 
 function Home() {
   return (
-    <div>
-      <div>
-        <h1>Are you starving</h1>
+    <div className='home p-5 h-screen'>
+        <h1 className='text-white font-extrabold text-3xl'>Are you starving?</h1>
         <p>Within a few clicks, find meals that are accessible near you</p>
-        
+
         <div>
-          <div>
-            <button></button>
+          <div className='flex justify-between items-center'>
+            <div className='flex justify-around items-center'>
+              <BiSolidShoppingBag />
+            <button>Deliver</button>
+            </div>
+            <div className='flex justify-around items-center'>
+              <GrBike />
+            <button>Pickup</button>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div>
-
-      </div>
     </div>
   )
 }
