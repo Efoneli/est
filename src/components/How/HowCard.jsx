19e57a159022b58e9fbae3f3d47efee44f795046
Efoneli <abbaefoneli@gmail.com>
2023-11-06 22:53:09 -0500

    cards

--- src/components/How/HowCard.jsx
+++ src/components/How/HowCard.jsx
@@ -5,10 +5,10 @@ function HowWorks({ data }) {
   return (
     <>
      
-        <div>
+        <div className="flex flex-col justify-around items-center m-6">
           <img src={photo} alt="imag" height="50px" width="100px" />
           <h2 className="text-sm font-bold p-1">{title}</h2>
-          <p className="text-xs">{desc} </p>
+          <p className="text-xs text-gray-400 py-2">{desc} </p>
         </div>
      
     </>
