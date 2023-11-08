454ba32aa664d8b88b420ea342295070b57a84f1
Efoneli <abbaefoneli@gmail.com>
2023-11-08 08:52:17 -0500

    today

--- src/pages/SearchFood.jsx
+++ src/pages/SearchFood.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { AiOutlineRight, AiOutlineLeft } from "react-icons/ai";
+import search1 from '../components/Assets/images/search1.png'
+import search2 from '../components/Assets/images/search2.png'
+import search3 from '../components/Assets/images/search3.png'
+import search4 from '../components/Assets/images/search4.png'
+import search5 from '../components/Assets/images/search5.png'
+import search6 from '../components/Assets/images/search6.png'
+
 
 
 function SearchFood() {
   return (
     <>
-        <div className='bg-orange-50'>
-            <div className='flex items-center justify-around'>
+        <div className='bg-orange-50 p-6'>
+            <div className='flex items-center justify-between px-10'>
                 <h2 className='font-extrabold text-lg'>Search by Food</h2>
                 <div className='flex '>
                     <div className='flex items-center text-yellowColor text-xs m-3'>
@@ -24,8 +31,36 @@ function SearchFood() {
                 </div>
             </div>
 
-        <div>
-            
+        <div className='grid grid-cols-3 my-8 p-5 phone:grid-cols-6'>
+            <div className='flex flex-col justify-start items-center'>
+                <img src={search1} alt="food" className='rounded-full h-[150px] w-[150px]' />
+                <p>Pizza</p>
+            </div>
+
+            <div className='flex flex-col justify-start items-center'>
+                <img src={search2} alt="food" className='rounded-full h-[150px] w-[150px]' />
+                <p>Pizza</p>
+            </div>
+
+            <div className='flex flex-col justify-start items-center'>
+                <img src={search3} alt="food" className='rounded-full h-[150px] w-[150px]' />
+                <p>Pizza</p>
+            </div>
+
+            <div className='flex flex-col justify-start items-center'>
+                <img src={search4} alt="food" className='rounded-full h-[150px] w-[150px]' />
+                <p>Pizza</p>
+            </div>
+
+             <div className='flex flex-col justify-start items-center'>
+                <img src={search5} alt="food" className='rounded-full h-[150px] w-[150px]' />
+                <p>Pizza</p>
+            </div>
+
+            <div className='flex flex-col justify-start items-center'>
+                <img src={search6} alt="food" className='rounded-full h-[150px] w-[150px]' />
+                <p>Pizza</p>
+            </div>
         </div>
 
         </div>
