cc3fc14e76d1505e95bedd6fb6b5cd5ca8ead411
Efoneli <abbaefoneli@gmail.com>
2023-11-08 20:38:29 -0500

    best deals

--- src/pages/BestDeals.jsx
+++ src/pages/BestDeals.jsx
@@ -3,32 +3,111 @@ import { AiOutlineRight } from "react-icons/ai";
 import best1 from "../components/Assets/images/best1.png";
 import best2 from "../components/Assets/images/best2.png";
 import best3 from "../components/Assets/images/best3.png";
+import order from "../components/Assets/images/order.png";
 
 function BestDeals() {
   return (
     <>
-      <div className="bg-white m-5">
-        <div className="max-w-2xl border border-gray-400 shadow-lg rounded-lg flex items-center justify-center">
-          <div className="">
-            <h2>
-              Best deals <span>Crispy Sandwiches</span>
-            </h2>
-            <p>
-              Enjoy the large size of sandwiches. Complete perfect slice of
-              sandwiches.
-            </p>
-
-            <button className="bg-gradient-to-r from-yellow-400 to-yellowColor text-white text-xs px-4 py-2 rounded-md flex items-center font-bold">
-              PROCEED TO ORDER
-              <AiOutlineRight className="" />
-            </button>
-          </div>
+      <div className="bg-white grid gap-6 place-items-center m-8 p-5">
+        <div className="max-w-sm shadow-xl rounded-lg flex flex-col items-center justify-center md:flex-row md:max-w-xl">
           <div>
-          <img src={best1} alt="deals" className="rounded-lg" />
+            <div className="p-5">
+              <h2 className="font-extrabold text-xl">
+                Best deals{" "}
+                <span className="text-yellowColor">Crispy Sandwiches</span>
+              </h2>
+              <p className="text-xs leading-4 text-gray-500">
+                Enjoy the large size of sandwiches. Complete perfect slice of
+                sandwiches.
+              </p>
+
+              <button className="bg-gradient-to-r from-yellowColor to-orangeColor shadow-md shadow-yellowColor text-white text-xs px-4 py-2 mt-7 rounded-md flex items-center font-bold">
+                PROCEED TO ORDER
+                <AiOutlineRight className="" />
+              </button>
+            </div>
+          </div>
+
+          <div className="">
+            <img
+              src={best1}
+              alt="deals"
+              className="h-[16.5rem] w-[64rem] rounded-b-lg object-fill md:rounded-r-lg"
+            />
           </div>
         </div>
 
+        <div className="max-w-sm shadow-xl rounded-lg flex flex-col-reverse items-center justify-center md:flex-row md:max-w-xl">
+          <div className="">
+            <img
+              src={best2}
+              alt="deals"
+              className="h-[16.5rem] w-[64rem] rounded-l-lg"
+            />
+          </div>
 
+          <div>
+            <div className="p-5">
+              <h2 className="font-extrabold text-xl">
+                Celebrate parties with{" "}
+                <span className="text-yellowColor">Fried Chicken</span>
+              </h2>
+              <p className="text-xs leading-4 text-gray-500">
+                Get the best fried chicken smeared with a lip smacking lemon
+                chili flavor. Check out best deals for fried chicken.
+              </p>
+
+              <button className="bg-gradient-to-r from-yellowColor to-orangeColor shadow-md shadow-yellowColor text-white text-xs px-4 py-2 mt-7 rounded-md flex items-center font-bold">
+                PROCEED TO ORDER
+                <AiOutlineRight className="" />
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="max-w-sm shadow-xl rounded-lg flex flex-col items-center justify-center md:flex-row md:max-w-xl">
+          <div>
+            <div className="p-5">
+              <h2 className="font-extrabold text-xl">
+                Wanna eat hot & spicy{" "}
+                <span className="text-yellowColor">Pizza?</span>
+              </h2>
+              <p className="text-xs leading-4 text-gray-500">
+                Pair up with a friend and enjoy the hot and crispy pizza pops.
+                Try it with the best deals.
+              </p>
+
+              <button className="bg-gradient-to-r from-yellowColor to-orangeColor shadow-md shadow-yellowColor text-white text-xs px-4 py-2 mt-7 rounded-md flex items-center font-bold">
+                PROCEED TO ORDER
+                <AiOutlineRight className="" />
+              </button>
+            </div>
+          </div>
+
+          <div className="">
+            <img
+              src={best3}
+              alt="deals"
+              className="h-[16.5rem] w-[64rem] rounded-r-lg"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="relative">
+        <div className="">
+        <img src={order} alt="order" />
+        </div>
+            <div className="absolute top-0 left-24 translate-x-12">
+            <h1 className="font-extrabold text-2xl text-white text-center">
+                Are you ready to order with <br />the best deals?
+            </h1>
+            <button className="bg-orangeColor text-white text-xs px-4 py-2 mt-7 rounded-md flex items-center justify-center font-bold">
+                PROCEED TO ORDER
+                <AiOutlineRight className="" />
+            </button>
+            </div>
+           
       </div>
     </>
   );
